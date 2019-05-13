65663029931a01e03328ca06088d29dbb399f012
Chenyanlan <31501391@stu.zucc.edu.cn>
2019-05-13 16:50:51 +0800

    对登录表单进行了简单的验证

--- bookstore/src/components/Login.vue
+++ bookstore/src/components/Login.vue
@@ -3,15 +3,15 @@
        用户名:<input/> <br>
        密码:<input/> <br>
   <button @click= "signin">点击登录</button>-->
-  <v-form direction="horizontal" class="login-form">
-    <v-form-item label="用户名">
-      <v-input type="text" placeholder="用户名" size="large"></v-input>
+  <v-form direction="horizontal" class="login-form" :rules= "rules" :model= "ruleForm" ref= "ruleForm">
+    <v-form-item label="用户名" prop = "name">
+      <v-input type="text" placeholder="用户名" size="large" v-model= "ruleForm.name"></v-input>
     </v-form-item>
-    <v-form-item label="密码" required>
-      <v-input type="password" placeholder="密码" size="large"></v-input>
+    <v-form-item label="密码" required prop="pass">
+      <v-input type="password" placeholder="密码" size="large" v-model = "ruleForm.pass"></v-input>
     </v-form-item>
     <v-form-item>
-      <v-button type="primary" @click= "signin">确定</v-button>
+      <v-button type="primary" @click= "signin" size = "large" icon = "smile-o"><span>确定</span></v-button>
     </v-form-item>
   </v-form>
   <!-- <v-form class="login-form">
@@ -34,9 +34,40 @@
 
 <script>
 export default {
+  data(){
+    return {
+      ruleForm:{
+        name:'',
+        pass:''
+      },
+      rules:{
+        name:[
+          {required:true,message:'用户名不能为空'}
+        ],
+        pass:[
+          {required:true,message:'密码不能为空'}
+        ]
+      }
+    }
+  },
   methods: {
     signin() {
-      this.$router.replace("/");
+
+      this.$refs.ruleForm.validate((valid)=>{
+        if(valid){
+          if(this.ruleForm.name==='user'&&this.ruleForm.pass==='111'){
+            this.$notification.success({
+              message:'成功登录!',
+              description:'欢迎你, '+this.ruleForm.name + '!'
+            })
+            this.$router.replace('/')
+          }else{
+          this.$message.error("用户名或密码错误")
+          }
+        }else{
+          return false
+        }
+      })
     }
   }
 };
